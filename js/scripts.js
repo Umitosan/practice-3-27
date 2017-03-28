d1caf42f888514640b1839611012dfd87deff5ca
Dominic Brown and Kat Gifford <kat@>
2017-03-27 17:35:28 -0700

    clean a bit

--- js/scripts.js
+++ js/scripts.js
@@ -23,8 +23,9 @@ $(document).ready(function() {
 
   $("#formOne").hide();
   });
-  // grocery list fuctions
 
+
+  // grocery list fuctions
   $("#formTwo").submit(function(event){
     event.preventDefault();
 
@@ -32,20 +33,22 @@ $(document).ready(function() {
     var splitWd = initialWd.split(" ");
     console.log(splitWd);
 
-    var wdThree = splitWd.map(function(tmpindex) {
+    var wdThree = [];
 
+    splitWd.map(function(tmpindex) {
       if (tmpindex.length <= 3) {
-        return tmpindex;
+        wdThree.push(tmpindex);
         console.log(tmpindex);
       } else {
-        console.log("not 3");
+        return 0;
       }
     });
 
     console.log("wdThree: " + wdThree);
+
     $("#poetry").text(initialWd);
+    $("#poetry2").text(wdThree);
 
   });
-
 
 });
